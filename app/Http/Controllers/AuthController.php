40e4c10c366c4c4b4599c3c2d0dea75d94e6172a
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-05-30 15:52:20 -0300

    add jwt add signup add new routes add login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        try {
+            $credentials = request(['email', 'password']);
+
+            if (!$token = JWTAuth::attempt(array('email' =>  $credentials['email'], 'password' => $credentials['password']))) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+
+            return response()->json(['success' => true, 'token' => $token, 'message' => 'CREATED'], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['success' => false, 'error' => $th, 'message' => 'ERROR'], 200);
+        }
+    }
+    /**
+     * Signup usuário
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function signup()
+    {
+        try {
+            $credentials = request(['name', 'email', 'password']);
+            $usuario = new User();
+            $usuario->name = $credentials['name'];
+            $usuario->email = $credentials['email'];
+            $usuario->password = app('hash')->make($credentials['password']);
+            $usuario->save();
+            return response()->json(["success" => true, "data" => $usuario, "message" => "CREATED"], 200);
+        } catch (\Throwable $th) {
+            return response()->json(["success" => false, "data" => $th, "message" => "CREATED"], 200);
+        }
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
